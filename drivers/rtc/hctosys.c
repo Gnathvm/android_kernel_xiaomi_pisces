9903d35dda92af28f3b2c9b3295230063f3b6575
David Fries <david@fries.net>
2012-10-04 17:14:12 -0700

    rtc_sysfs_show_hctosys(): display 0 if resume failed
    Without this patch /sys/class/rtc/$CONFIG_RTC_HCTOSYS_DEVICE/hctosys
    contains a 1 (meaning "This rtc was used to initialize the system
    clock") even if setting the time by do_settimeofday() at bootup failed.
    The RTC can also be used to set the clock on resume, if it did 1,
    otherwise 0.  Previously there was no indication if the RTC was used
    to set the clock in resume.
    
    This uses only CONFIG_RTC_HCTOSYS_DEVICE for conditional compilation
    instead of it and CONFIG_RTC_HCTOSYS to be more consistent.
    rtc_hctosys_ret was moved to class.c so class.c no longer depends on
    hctosys.c.
    
    [sfr@canb.auug.org.au: fix build]
    Signed-off-by: David Fries <David@Fries.net>
    Cc: Matthew Garrett <mjg@redhat.com>
    Cc: Alessandro Zummo <a.zummo@towertech.it>
    Cc: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/rtc/hctosys.c
+++ drivers/rtc/hctosys.c
@@ -23,8 +23,6 @@
  * the best guess is to add 0.5s.
  */
 
-int rtc_hctosys_ret = -ENODEV;
-
 static int __init rtc_hctosys(void)
 {
 	int err = -ENODEV;
@@ -57,7 +55,7 @@ static int __init rtc_hctosys(void)
 
 	rtc_tm_to_time(&tm, &tv.tv_sec);
 
-	do_settimeofday(&tv);
+	err = do_settimeofday(&tv);
 
 	dev_info(rtc->dev.parent,
 		"setting system clock to "
