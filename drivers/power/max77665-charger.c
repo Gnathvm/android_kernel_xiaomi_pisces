09dc04038ea86453d78b9a06aa8931d374aa472f
Xuefer <xuefer@gmail.com>
2016-08-26 01:01:48 +0800

    power: USB force fast charging
    Use base from Chad Froebel <chadfroebel@gmail.com> so we continue to use the same generic sysfs mechanism
    
    Off by default
    Turn on: echo 1 > /sys/kernel/fast_charge/force_fast_charge
    Turn off: echo 0 > /sys/kernel/fast_charge/force_fast_charge
    
    Initial ported to pisces by Xuefer
    
    Signed-off-by: Jean Francis Dominique S. Mabalot <jmabalot@google.com>
    Signed-off-by: Xuefer <xuefer@gmail.com>

--- drivers/power/max77665-charger.c
+++ drivers/power/max77665-charger.c
@@ -37,6 +37,7 @@
 #include <linux/mutex.h>
 #include <linux/delay.h>
 #include <linux/debugfs.h>
+#include <linux/fastchg.h>
 
 /* IDBEN register define for MUIC
  * it's better to put it in extcon-max77665.c
@@ -591,6 +592,12 @@ static int max77665_enable_charger(struct max77665_charger *charger,
 	if (true == extcon_get_cable_state(edev, "USB-Host")) {
 		mode = OTG;
 		charger->max_current_mA = 0;
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	} else if (force_fast_charge == FAST_CHARGE_FORCE_AC) {
+		mode = CHARGER;
+		charger->ac_online = 1;
+		charger->max_current_mA = 2200;
+#endif
 	} else if (true == extcon_get_cable_state(edev, "USB")) {
 		mode = CHARGER;
 		charger->usb_online = 1;
@@ -918,7 +925,11 @@ static void charger_monitor_worker(struct work_struct *work)
 	}
 
 	/* Is the battery normal */
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	mA = force_fast_charge ? 2000 : charger->plat_data->fast_chg_cc;
+#else
 	mA = charger->plat_data->fast_chg_cc;
+#endif
 	voltage = charger->plat_data->term_volt;
 	if (batt_temp_is_normal(batt_temp) &&
 				(mA != charger->fast_chg_cc || voltage != charger->term_volt)) {
