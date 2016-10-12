bab4a3663f33bca9cdc8609bb387a9cab215ae01
Xuefer <xuefer@gmail.com>
2016-10-12 11:23:17 +0800

    reset cgroup to android-3.4, fix /acct rmdir hang
    Signed-off-by: Xuefer <xuefer@gmail.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -391,8 +391,7 @@ static void free_css_set_work(struct work_struct *work)
 		 * rcu_read_lock is used to keep it alive.
 		 */
 		rcu_read_lock();
-		if (atomic_dec_and_test(&cgrp->count) &&
-		    notify_on_release(cgrp)) {
+		if (atomic_dec_and_test(&cgrp->count)) {
 			check_for_release(cgrp);
 			cgroup_wakeup_rmdir_waiter(cgrp);
 		}
@@ -1926,15 +1925,6 @@ int cgroup_attach_task(struct cgroup *cgrp, struct task_struct *tsk)
 				 */
 				failed_ss = ss;
 				goto out;
-			}
-		} else if (!capable(CAP_SYS_ADMIN)) {
-			const struct cred *cred = current_cred(), *tcred;
-
-			/* No can_attach() - check perms generically */
-			tcred = __task_cred(tsk);
-			if (cred->euid != tcred->uid &&
-			    cred->euid != tcred->suid) {
-				return -EACCES;
 			}
 		}
 	}
